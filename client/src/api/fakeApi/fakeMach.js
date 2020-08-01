6873e6bd100c207c2cc7e1ee8ce294f1abad9fa7
NEDA\Neda <n.taherifar@gmail.com>
2020-08-01 21:59:39 +0800

    updating sensor queries. parsing time string to time elapsed.

--- client/src/api/fakeApi/fakeMach.js
+++ client/src/api/fakeApi/fakeMach.js
@@ -1,21 +1,22 @@
 import mach from "./sensordata.json";
+import * as ST from "../../settings/settings";
 
 const machines = mach;
 
 export const getAllMachines = () => {
-  return machines.map(mach => {
+  return machines.map((mach) => {
     return {
       lat: mach.coordinate.lat,
       lng: mach.coordinate.lng,
       name: mach.name,
       id: mach.machId,
-      numSensors: mach.sensors.length
+      numSensors: mach.sensors.length,
     };
   });
 };
 
-export const getMachineById = mach_id => {
-  return machines.find(mach => {
+export const getMachineById = (mach_id) => {
+  return machines.find((mach) => {
     return mach.machId === mach_id;
   });
 };
@@ -25,7 +26,43 @@ export const getMachineByTimeInterval = () => {};
 export const getAllSensors = () => {};
 
 export const getSensorsByID = (selectedMach, sensId) => {
-  return selectedMach.sensors.find(o => o.sensId === sensId);
+  let sensor = selectedMach.sensors.find((o) => o.sensId === sensId);
+
+  let t0 = 0;
+  const elapsedTime = sensor.histData.map((data, index) => {
+    let t = new Date(data.timestamp);
+    if (index === 0) {
+      t0 = t;
+    }
+
+    let dt = t - t0;
+    let diffMins = Math.round((dt / 3600000) * 60);
+    switch (sensor.senstype) {
+      case ST.SENSE_TYPES.PRES:
+        return {
+          t: diffMins,
+          P: data.value.P,
+        };
+      case ST.SENSE_TYPES.TEMP:
+        return {
+          t: diffMins,
+          T: data.value.T,
+        };
+      case ST.SENSE_TYPES.VIBR:
+        return {
+          t: diffMins,
+          X: data.value.X,
+          Vel: data.value.Vel,
+          Acc: data.value.Acc,
+        };
+    }
+  });
+
+  sensor.histData = elapsedTime;
+
+  return sensor;
 };
+
+const parseDateTime = (DateTime) => {};
 
 export const getSensorsByMachId = () => {};
--- client/src/components/Plot/LineChart/PlotLineChart.js
+++ client/src/components/Plot/LineChart/PlotLineChart.js
@@ -7,18 +7,35 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
-  ResponsiveContainer
+  ResponsiveContainer,
 } from "recharts";
 import { connect } from "react-redux";
 import * as ST from "../../../settings/settings";
 import TestData from "./testData.json";
+import * as Api from "../../../api/fakeApi/fakeMach";
 
 class PlotLineChart extends Component {
   state = {};
 
   render() {
+    const colors = ["#1E8449", "#E74C3C", "#7D3C98"];
     let chart = "Select Machine";
     if (this.props.dataChart) {
+      const lines = Object.keys(this.props.dataChart[0]).map((k, i) => {
+        if (k !== "t") {
+          return (
+            <Line
+              type="linear"
+              dataKey={k}
+              stroke={colors[i]}
+              strokeWidth="2"
+              dot={false}
+              activeDot={{ r: 5 }}
+            />
+          );
+        }
+      });
+
       chart = (
         <ResponsiveContainer width="95%" height="95%">
           <LineChart
@@ -29,22 +46,24 @@ class PlotLineChart extends Component {
               top: 5,
               right: 30,
               left: 20,
-              bottom: 5
+              bottom: 5,
             }}
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="x" />
+            <XAxis dataKey="t" />
             <YAxis />
             <Tooltip />
             <Legend />
-            <Line
+            {/* <Line
               type="linear"
-              dataKey="y"
+              dataKey="y1"
               stroke="#117A65"
               strokeWidth="2"
               dot={false}
               activeDot={{ r: 10 }}
-            />
+            /> */}
+
+            {lines}
           </LineChart>
         </ResponsiveContainer>
       );
@@ -53,47 +72,53 @@ class PlotLineChart extends Component {
   }
 }
 
-const mapStateToProps = state => {
-  const testSensor = state.mach.selectedMach
-    ? state.mach.selectedMach.sensors[0]
+const mapStateToProps = (state) => {
+  const sensID = state.mach.selectedMach
+    ? state.mach.selectedMach.machId.toString() + "_1"
     : null;
+  // const testSensor = state.mach.selectedMach
+  //   ? state.mach.selectedMach.sensors[0]
+  //   : null;
 
   let dataChart = null;
-  if (testSensor) {
-    switch (testSensor.senstype) {
-      case ST.SENSE_TYPES.PRES:
-        dataChart = testSensor.histData.map((data, index) => {
-          return {
-            // x: data.timestamp,
-            x: index,
-            y: data.value.P
-          };
-        });
-        break;
-      case ST.SENSE_TYPES.TEMP:
-        dataChart = testSensor.histData.map((data, index) => {
-          return {
-            // x: data.timestamp,
-            x: index,
-            y: data.value.T
-          };
-        });
-        break;
-      case ST.SENSE_TYPES.VIBR:
-        dataChart = testSensor.histData.map((data, index) => {
-          return {
-            // x: data.timestamp,
-            x: index,
-            y: data.value.Acc
-          };
-        });
-        break;
-    }
+  let testSensor = null;
+  if (sensID) {
+    testSensor = Api.getSensorsByID(state.mach.selectedMach, sensID);
+    // switch (testSensor.senstype) {
+    //   case ST.SENSE_TYPES.PRES:
+    //     dataChart = testSensor.histData.map((data, index) => {
+    //       console.log(data.timestamp.substring(1, 4));
+    //       return {
+    //         // x: data.timestamp,
+    //         x: index,
+    //         y: data.value.P,
+    //       };
+    //     });
+    //     break;
+    //   case ST.SENSE_TYPES.TEMP:
+    //     dataChart = testSensor.histData.map((data, index) => {
+    //       return {
+    //         // x: data.timestamp,
+    //         x: index,
+    //         y: data.value.T,
+    //       };
+    //     });
+    //     break;
+    //   case ST.SENSE_TYPES.VIBR:
+    //     dataChart = testSensor.histData.map((data, index) => {
+    //       return {
+    //         // x: data.timestamp,
+    //         x: index,
+    //         y: data.value.Acc,
+    //       };
+    //     });
+    //     break;
+    // }
   }
 
   return {
     testSensor: testSensor,
-    dataChart: dataChart
+    dataChart: testSensor ? testSensor.histData : null,
   };
 };
 
